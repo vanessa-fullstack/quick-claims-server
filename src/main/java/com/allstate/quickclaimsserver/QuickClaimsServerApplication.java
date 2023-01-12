becf954b46eeb3fa7434e73a825a4dcaa87f288b
Vanessa <vanessacarney_@hotmail.co.uk>
2023-01-12 13:04:03 +0000

    first commit

---
+++ src/main/java/com/allstate/quickclaimsserver/QuickClaimsServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.quickclaimsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickClaimsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickClaimsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/quickclaimsserver/QuickClaimsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.quickclaimsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickClaimsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
